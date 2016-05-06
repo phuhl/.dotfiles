1958ab20164943828c75e2f026151f5a146809b9
Philipp Uhl <philipp.uhl@rwth-aachen.de>
2016-05-06 14:22:06 +0200

    New layout for sxhkd + many smaller changes

--- system/launch_linux_desktop.sh
+++ system/launch_linux_desktop.sh
@@ -4,7 +4,8 @@
 nitrogen --restore &
 compton --config $HOME/.config/compton/compton.conf -cGb --opengl --sw-opti --vsync opengl-swc &
 numlockx &
-stalonetray &
 dunst &
 $HOME/.dotfiles/system/panel &
+sleep 5
+stalonetray &
 wait
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -37,4 +37,4 @@ setopt HIST_REDUCE_BLANKS
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
 setopt complete_aliases
-EDITOR="emacs"
+EDITOR="eq"
