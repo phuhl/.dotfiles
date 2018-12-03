b97487ce50bd3089c93930f686051a350a168033
Philipp Uhl <git@ph-uhl.com>
2018-12-03 16:57:41 +0100

    Screenshots

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias ls='ls --color=auto'
 alias sudo='sudo env "PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/home/adminu/documents/bin/bash/bspwm/:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/adminu/.dotfiles/bin:/home/adminu/.dotfiles/system" '
 
 alias sudo='sudo env "PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/philipp/.dotfiles/bin:/home/philipp/.dotfiles/system" '
+
+alias rewifi="bash -c \"sudo restartDhcpd\""
