93808b467f7d13bc0eeeb1ad3e4fc1c20df22e2f
Philipp Uhl <git@ph-uhl.com>
2019-04-01 13:35:35 +0200

    Fixes tor-download script

--- tor/install.sh
+++ tor/install.sh
@@ -5,6 +5,7 @@ echo ''
 mkdir ../../tor
 mkdir ../../tor/downloads
 chown $(stat -c '%U' ../../) ../../tor/downloads
+chown $(stat -c '%U' ../../) ../../tor
 cp updateTor.py ../../tor
 cp updateTor /etc/cron.weekly
 chmod 744 /etc/cron.weekly
