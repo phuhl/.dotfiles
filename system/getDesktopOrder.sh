8d6c5b6b331ca77846b78a10a448ddcb69720ff2
Philipp Uhl <git@ph-uhl.com>
2019-08-08 09:34:42 +0200

    Adds "better" multi monitor support

--- system/getDesktopOrder.sh
+++ system/getDesktopOrder.sh
@@ -1,4 +1,11 @@
-CONFIG=$(cat $HOME/.config/screens)
+CONNECTED=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+NUM_OF_DISPLS=$(echo $CONNECTED | wc -w)
+CONFIG=$(cat $HOME/.config/screen)
+
+if [[ $NUM_OF_DISPLS > 1 ]]; then
+    CONFIG=$(cat $HOME/.config/screens)
+fi
+
 desktops=""
 while read config; do
    monitor=$(echo $config | awk '{$1 = ""; print $0;}')
